80098fcbc2efb62fd3c5443be142dfcae82a9e89
matheusrebola <m.cavalcantir@outlook.com>
2024-07-17 23:26:44 -0300

    feat: criação do projeto spring

---
+++ hotel-booking-spring/src/main/java/hotelbooking/HotelBookingSpringApplication.java
@@ -0,0 +1,13 @@
+package hotelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingSpringApplication.class, args);
+	}
+
+}
---
+++ hotel-booking-spring/src/test/java/hotelbooking/HotelBookingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hotelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
