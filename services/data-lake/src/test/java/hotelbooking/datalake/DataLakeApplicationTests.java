bb83ed3d37982d20b89f2f9d2330897b714bc415
Matheus Rebola <m.cavalcantir@outlook.com>
2025-08-22 15:44:35 -0300

    feat: configuração da arquitetura básica do projeto

---
+++ services/data-lake/src/test/java/hotelbooking/datalake/DataLakeApplicationTests.java
@@ -0,0 +1,13 @@
+package hotelbooking.datalake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/hoteis/src/main/java/hotelbooking/hoteis/HoteisApplication.java
@@ -0,0 +1,13 @@
+package hotelbooking.hoteis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteisApplication.class, args);
+	}
+
+}
